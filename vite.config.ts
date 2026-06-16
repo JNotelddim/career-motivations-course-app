321129ba44d1ae7099a6ea8fa9a7c1e03181b74c
Jared McKimm <jared.mckimm@gmail.com>
2026-06-16 13:46:04 -0700

    deploy config: adjust build base path for deployed url subpath

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
+  base: "/jared/career-motivations-worksheet/",
   resolve: {
     tsconfigPaths: true,
   },
