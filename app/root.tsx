430613db027745f46143529393217d7034e99728
Jared McKimm <jared.mckimm@gmail.com>
2026-06-17 11:10:27 -0700

    build: add sites sdk script

--- app/root.tsx
+++ app/root.tsx
@@ -31,6 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <script src="/sdk/v1/sites.js"></script>
       </head>
       <body>
         {children}
