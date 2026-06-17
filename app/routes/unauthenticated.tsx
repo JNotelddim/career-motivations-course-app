795d4e8eddd8a504f6c9c0c907b7734d15e25210
Jared McKimm <jared.mckimm@gmail.com>
2026-06-17 15:39:17 -0700

    fix: create redirect away from "unauthenticated" page in authenticated case

--- app/routes/unauthenticated.tsx
+++ app/routes/unauthenticated.tsx
@@ -1,5 +1,8 @@
+import { useEffect } from "react";
 import type { Route } from "./+types/unauthenticated";
 import { Unauthenticated } from "~/components/page/unauthenticated";
+import { useAuth } from "~/components/providers/authProvider";
+import { useNavigate } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +12,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function UnauthenticatedRoute() {
+  const { status } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect( () => {
+    if(status === "authenticated") {
+      navigate("/", { replace: true });
+    }
+  }, [status])
+
   return <Unauthenticated />;
 }
